29e730dd05206b032ae1026325ceb4d959e99501
cluis11 <cluis.9405@gmail.com>
2014-05-23 12:46:18 -0600

    Update Tarea_Programada.py

--- app/Tarea_Programada.py
+++ app/Tarea_Programada.py
@@ -12,6 +12,7 @@ def Cargar_Base():
         prolog.assertz(str)
         str = BaseC.readline()
     BaseC.close()
+    
 #Direcciones de la aplicacion Web
 #Direccion de la pagina principal
 @app.route('/') 
@@ -63,7 +64,7 @@ def Ingresa_Platillo(Restaurante,Platillo,Sabor,Pais,Ingredientes):
     BaseC.write(agregar_Platillo+"\n")
     BaseC.close()
 
-
+#Direccion donde se accede al menu de consultas
 @app.route('/Consulta de restaurante')
 def Consultas():
     BaseC = open("BaseC.txt", "a+")
@@ -71,32 +72,19 @@ def Consultas():
     if linea =='':
         return render_template('Vacia.html')
     else:
-        return render_template('lista_consultas.html')
+        return render_template('Lista_Consultas.html')
     BaseC.close()
 
-
-#Consultas
-def NoEsta_enLista(Lista,elemento):
-    for i in Lista:
-        if i == elemento:
-            return False
-        else:
-            return True
-
-
+#Direccion de consulta de lista de restaurantes
 @app.route('/Consulta de restaurantes')
-def Consulta_de_restaurantes():
-    
-    ListaRest = []
-    for i in prolog.query("restaurante(Restaurante,_,_,_,_)"):
-        if ListaRest == []:
-            ListaRest.append(i ["Restaurante"])
-        elif NoEsta_enLista(ListaRest, i ["Restaurante"]):
-            ListaRest.append(i ["Restaurante"])
-    if ListaRest == []:
+def Consulta_de_restaurantes():    
+    ListaRestaurantes = []
+    for r in prolog.query("restaurante(Restaurante,_,_,_,_)"):
+		ListaRestaurantes=ListaRestaurante+[r["Restaurante"]]
+    if ListaRestaurantes == []:
         return render_template('Vacia.html')
     else:
-        restaurante = list(ListaRest)
+        restaurante = list(ListaRestaurantes)
     	return render_template('Consulta.html',restaurante=restaurante)
     
 #Consulta del restaurane por nombre
@@ -104,107 +92,104 @@ def Consulta_de_restaurantes():
 def Rest_por_Nombres():
     return render_template('Rest_por_Nombres.html')
 
+#Muestra la info del restaurante a buscar
 @app.route('/Consulta nombre del restaurante', methods=['POST'])
 def Consulta_res_por_nombre():
     restaurante =  request.form['restaurante']
-    NombreRest = restaurante.lower()
-    ListaRest = []
+    restaurante = restaurante.lower()
+    ListaRestaurante = []
     for i in prolog.query( "restaurante("+NombreRest+",TipoComida,Ubicacion,Telefono,Horario)"):
-            ListaRest.append(i["Ubicacion"])
-            ListaRest.append(i["TipoComida"])
-            ListaRest.append(i["Telefono"])
-            ListaRest.append(i["Horario"])
-    if ListaRest == []:
+            ListaRestaurante=ListaRestaurante+[i["Ubicacion"]]
+            ListaRestaurante=ListaRestaurante+[i["TipoComida"]]
+            ListaRestaurante=ListaRestaurante+[i["Telefono"]]
+            ListaRestaurante=ListaRestaurante+[i["Horario"]]
+    if ListaRestaurante == []:
         return render_template('Vacia.html')
     else:
-    	nombre = list (ListaRest )
+    	nombre = list (ListaRestaurante)
     	return render_template('Consulta_nombre.html',nombre=nombre) 	
    
-#####################################################################################
 #Consulta del restaurane por tipo de comida
 @app.route('/Consulta de tipo de comida')
 def Rest_por_Tipo():
     return render_template('Rest_por_Tipo.html')
 
-
+#Lista de restaurantes por ripo de comida
 @app.route('/Consulta nombre del tipo de comida', methods=['POST'])
 def Consulta_Rest_por_Tipo():
-    tipo_comida = request.form['tipo_comida']
-    ListaRest = []
-    Comida=tipo_comida.lower()
+    tipo = request.form['tipo_comida']
+    tipo=tipo.lower()
+    ListaRestaurantes = []
     for i in prolog.query( "restaurante(Restaurante,"+Comida+",_,_,_)"):
-        if ListaRest == []:
-            ListaRest.append(i ["Restaurante"])
-        elif NoEsta_enLista(ListaRest, i ["Restaurante"]):
-            ListaRest.append(i ["Restaurante"])
-    if ListaRest == []:
+		ListaRestaurante=Listarestaurante+[i["Restaurante"]]
+    if ListaRestaurante == []:
         return render_template('Vacia.html')
     else:
-    	tipo= list(ListaRest)
+    	tipo= list(ListaRestaurante)
     	return render_template('Consulta_tipocomida.html',tipo=tipo)
-#Consulta de los ingredientes de platillos de un restaurante
+    	
+#Consulta de los ingredientes de un platillos especifico
 @app.route('/Consulta de los ingredientos')
 def Platillos_ingredientes():
     return render_template('Platillos_ingredientes.html')
 
+#Lista de ingredientes de un platillos
 @app.route('/Consulta por ingrediente', methods=['POST'])
-def Consulta_Rest_por_ingrediente():
+def Consulta_Restaurante_por_ingrediente():
     restaurante = request.form['restaurante']
     ingrediente = request.form['ingrediente']
-    
-    rest=restaurante.lower()
-    ing=ingrediente.lower()
-    ListaPlat=[]
-    for plato in prolog.query("platillo("+rest+",Platillo,_,_,Ingredientes)"):
-        Lista = plato["Ingredientes"]
-        for i in Lista:
-            if str(i)==ing:
-                ListaPlat.append(plato["Platillo"])
-    if ListaRest == []:
+    restaurante=restaurante.lower()
+    ingrediente=ingrediente.lower()
+    ListaPlatillos=[]
+    for p in prolog.query("platillo("+restaurante+",Platillo,_,_,Ingredientes)"):
+        ListaIngredientes = p["Ingredientes"]
+        for i in ListaIngredientes:
+            if str(i)==ingrediente:
+                ListaPlatillos=ListaPlatillos+[p["Platillo"]]
+    if ListaRestaurante == []:
         return render_template('Vacia.html')
     else:
-    	ingrediente_select = list(ListaPlat)
+    	ingrediente_select = list(ListaPlatillos)
     	return render_template('Consulta_ingrediente.html',ingrediente_select=ingrediente_select)
+    	
 ##Consulta de los platillos del restaurante
 @app.route('/Consulta platillos del restaurante')
-def Platillos_qTiene_Rest():
-    return render_template('Platillos_qTiene_Rest.html')
+def Platillos_Tiene_Rest():
+    return render_template('Platillos_Tiene_Rest.html')
 
+#Platillos de un restaurantes
 @app.route('/Consulta por platillo', methods=['POST'])
 def Platillos_Rest():
     restaurante = request.form['restaurante']
-
-    restaurant=restaurante.lower()
-    ListaPlat=[]
-    RestaurantePlatillo = list(prolog.query("platillo("+restaurant+",Platillo,_,_,_)"))
-    for restaurant in prolog.query( "platillo("+restaurant+",Platillo,_,_,_)"):
-        ListaPlat.append(restaurant["Platillo"])
-    if ListaRest == []:
+    restaurante=restaurante.lower()
+    ListaPlatillos=[]
+    RestaurantePlatillo = list(prolog.query("platillo("+restaurante+",Platillo,_,_,_)"))
+    for r in prolog.query( "platillo("+restaurante+",Platillo,_,_,_)"):
+        ListaPlatillos=ListaPlatillos+[r["Platillo"]]
+    if ListaPlatillos == []:
         return render_template('Vacia.html')
     else:
-    	restaurante_platillo= list (ListaPlat)
+    	restaurante_platillo= list (ListaPlatillos)
     	return render_template('Consulta_por_platillo_en_rest.html',restaurante_platillo=restaurante_platillo)
 
 
-##Consulta de los restaurantes segun el pais el cual es el platillo
+#Consulta de platillos dependiendo de su pais nativo
 @app.route('/Consulta por platillo de paises')
-def Rest_qTiene_PlatilloPais():
+def Rest_Tiene_PlatilloPais():
     return render_template('Rest_qTiene_PlatilloPais.html')
 
+#Consulta de restaurantes con un platillo de un pais especifico 
 @app.route('/Consulta por pais', methods=['POST'])
 def Rest_Platillo_Pais():
-    lugar = request.form['pais']
-    pais=lugar.lower()
-    ListaRest=[]
-    for restaurante in prolog.query( "platillo(Restaurante,_,_,"+pais+",_)"):
-        if ListaRest == []:
-            ListaRest.append(restaurante["Restaurante"])
-        elif NoEsta_enLista(ListaRest, restaurante["Restaurante"]):
-            ListaRest.append(restaurante["Restaurante"])
-    if ListaRest == []:
+    pais=request.form['pais']
+    pais=pais.lower()
+    ListaRestaurantes=[]
+    for r in prolog.query( "platillo(Restaurante,_,_,"+pais+",_)"):
+		ListaRestestaurante=ListaRestaurante+[r["Restaurante"]]
+    if ListaRestaurantes==[]:
         return render_template('Vacia.html')
     else:
-    	pais= list(ListaRest)
+    	pais= list(ListaRestaurantes)
     	return render_template('Consulta_pais_ingredientes.html',pais=pais)
 
 #LLamado para cargar la base de conocimientos
